87ee969be5435fcc88144472356efd4cbe739290
foviedomaltez <f.oviedo.maltez@accenture.com>
2016-12-16 12:09:21 -0600

    Inital commit

---
+++ src/com/ejemplo/MiClase.java
@@ -0,0 +1,22 @@
+package com.ejemplo;
+ 
+
+public class MiClase {
+	
+	
+	public static void main(String[] args) {
+		System.out.println("Probando Ant y Eclipse!");
+	}
+	
+	public String method1(){
+		
+		
+		return "Hello world";
+	}
+	
+	public int  method2(){
+		
+		
+		return 1;
+	}
+}
---
+++ src/com/ejemplo/MiClaseTest.java
@@ -0,0 +1,27 @@
+package com.ejemplo;
+
+import static org.junit.Assert.*;
+
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+public class MiClaseTest {
+
+	MiClase  obj = new MiClase();
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception {
+	}
+
+	@Test
+	public void test1() {
+		
+		 assertEquals("Hello world", obj.method1());
+	}
+	
+	@Test
+	public void test2() {
+		
+		 assertEquals(1, obj.method2());
+	}
+
+}
